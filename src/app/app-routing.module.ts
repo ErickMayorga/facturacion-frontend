2bf5d367a2921ae7970baa8aee244595b045a08c
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-06-30 17:45:24 -0500

    feat: Login and Not Found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RutaLoginComponent} from "./rutas/ruta-login/ruta-login.component";
+import {RutaNotFoundComponent} from "./rutas/ruta-not-found/ruta-not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: RutaLoginComponent
+  },
+  {
+    path: '**',
+    component: RutaNotFoundComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(
+    routes,
+    {useHash: true}
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RutaLoginComponent } from './rutas/ruta-login/ruta-login.component';
+import { RutaNotFoundComponent } from './rutas/ruta-not-found/ruta-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RutaLoginComponent,
+    RutaNotFoundComponent,
   ],
   imports: [
     BrowserModule,
